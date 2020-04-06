4b7131a2fe5b71035935d15a61bd6d86a1b28456
jyq <jiayuqi001023@sjtu.edu.cn>
2020-04-06 23:26:18 +0800

    modify

--- build.bash
+++ build.bash
@@ -4,4 +4,4 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-find ./src -name *.java | javac -d bin -classpath "/lib/antlr-4.8-complete.jar" @/dev/stdin
+find ./src -name *.java | javac -d bin -classpath "./lib/antlr-4.8-complete.jar" @/dev/stdin
