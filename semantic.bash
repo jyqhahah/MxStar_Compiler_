494c2c8d1c0dcf62771952acf58625bbe50440d9
jyq <jiayuqi001023@sjtu.edu.cn>
2020-04-06 23:29:25 +0800

    modify

--- semantic.bash
+++ semantic.bash
@@ -5,6 +5,6 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.8-complete.jar:./bin main"
+export CCHK="java -classpath ./lib/antlr-4.8-complete.jar:./bin Main"
 # cat > program.txt   # save everything in stdin to program.txt
 $CCHK
