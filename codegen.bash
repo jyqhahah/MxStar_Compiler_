08de1bc2197880598ab24f504d102aa044f73870
jyq <jiayuqi001023@sjtu.edu.cn>
2020-05-26 00:13:19 +0800

    codegen

--- codegen.bash
+++ codegen.bash
@@ -1,5 +1,4 @@
 set -e
 cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.8-complete.jar:./bin Main 1"
-#cat > code.txt   # save everything in stdin to program.txt
 $CCHK
--- semantic.bash
+++ semantic.bash
@@ -5,6 +5,5 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.8-complete.jar:./bin Main"
-# cat > program.txt   # save everything in stdin to program.txt
+export CCHK="java -classpath ./lib/antlr-4.8-complete.jar:./bin Main 0"
 $CCHK
